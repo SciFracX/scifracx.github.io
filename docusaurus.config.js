5c4bd8781c4ff818e247ec8b5062a2770358c8a4
ERIK <52615090+ErikQQY@users.noreply.github.com>
2022-01-07 12:49:22 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,9 +43,9 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'SciFracX',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'SciFracX Logo',
           src: 'img/logo.svg',
         },
         items: [
@@ -68,22 +68,14 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Documents',
+                to: 'docs',
               },
             ],
           },
           {
             title: 'Community',
             items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
               {
                 label: 'Twitter',
                 href: 'https://twitter.com/scifracx',
@@ -104,7 +96,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} SciFracX. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
