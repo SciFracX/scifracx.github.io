ab1100ff376c68e4eaea2d00284cc704719af99c
ErikQQY <2283984853@qq.com>
2022-01-07 23:50:53 +0800

    Add blog
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,8 +16,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="docs">
+            Let's start!
           </Link>
         </div>
       </div>
@@ -29,8 +29,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`SciFracX`}
+      description="Fractional Order Computing and Modeling">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
